ecdf998b535ee78a6eb74c39ae16ac7ae8ed2dce
puntitetra-ship-it <puntitetra@gmail.com>
2026-09-18 16:16:37 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "at.pvmonitor.v3"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "at.pvmonitor.v3"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "3.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
